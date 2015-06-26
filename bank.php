eaded2333cd2844ed4830b01c41d96da30780d7d
Hon Lam <lamh85@gmail.com>
2015-06-25 23:43:19 -0700

    Started classes and experiments with string interpolation.

---
+++ bank.php
@@ -0,0 +1,46 @@
+<?php
+
+  class Person {
+    var $name;
+    var $gender;
+    var $age;
+    var $cash = 0;
+
+    function say_hi($receiver) {
+      return 'Hello $receiver.';
+    }
+
+    function gain_money($amount) {
+      $cash += $amount;
+      echo "$name now has $cash";
+    }
+
+    function give_money($amount,$receiver) {
+
+    }
+
+  }
+
+  class Account {
+    var $owner;
+    var $balance = 0;
+    var $bank;
+    var $name;
+
+    function deposit($amount) {
+      $balance += $amount;
+    }
+
+    function transfer_out($amount,$receiver) {
+      $balance -= $amount;
+      $receiver += $amount;
+      echo "The account named $name now has $balance";
+    }
+  }
+
+$my_var = 'foobar';
+
+echo "I am using DOUBLE quotes: $my_var \n";
+echo 'I am using SINGLE quotes: $my_var'."\n";
+
+?>

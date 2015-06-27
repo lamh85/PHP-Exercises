6f0c3dfeb050d1ff4f26a7b8e33cef0d417b9288
Hon Lam <lamh85@gmail.com>
2015-06-27 15:33:49 -0700

    Started using constructor functions for nhl.php

--- nhl.php
+++ nhl.php
@@ -4,6 +4,14 @@
   // //////
   
   class Person {
+    public $name;
+    public $birthyear;
+    public $manager;
+
+    public function __construct($name,$birthyear) {
+      $this->name = $name;
+      $this->birthyear = $birthyear;
+    }
   }
 
   $team_member = new Person();

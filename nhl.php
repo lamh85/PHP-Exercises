989f191e1328240863e3f49b45af411f40498703
Hon Lam <lamh85@gmail.com>
2015-06-27 01:57:32 -0700

    Created nhl.php

---
+++ nhl.php
@@ -0,0 +1,22 @@
+<?php
+
+  // Person
+  // //////
+  
+  class Person {
+  }
+
+  $team_member = new Person();
+
+  $player = new Person();
+  $general_manager 
+
+  // Groups
+  // //////
+
+  class Group {
+  }
+
+  $team = new Group();
+
+?>

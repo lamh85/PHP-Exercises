d41a1a9aac639fdfafc8f2fd392d64660678477e
Hon Lam <lamh85@gmail.com>
2015-06-28 00:02:41 -0700

    Started experimenting with class inheritance.

--- nhl.php
+++ nhl.php
@@ -7,14 +7,23 @@
     public $name;
     public $birthyear;
     public $manager;
+    public $salary;
 
-    public function __construct($name,$birthyear) {
+    // I am the equivalent of a class method in Ruby: self.method_name
+    public static $species = "human";
+
+    public function __construct($name) {
       $this->name = $name;
-      $this->birthyear = $birthyear;
     }
   }
 
-  $team_member = new Person();
+  class Player extends Person {
+    public $number;
+    public $team;
+    public $handedness; // Shoots left or right
+    public $position;
+
+  }
 
   $player = new Person();
   $general_manager 
